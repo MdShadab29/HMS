e849c0fb68bd07c28d9607f736f8bfdefb30c976
Md Shadab <shadab.shadabi786@gmail.com>
2025-10-31 22:03:37 +0530

    Added Render domain to ALLOWED_HOSTS

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gyif@esxk7$h&wb8kt4#jkz_$qp+7f2megy!$s8dj*d(bwcxvl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hms-gt67.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
