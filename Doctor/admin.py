25ad77bff250a9ab772559d2cf2fa430f860dd93
Md Shadab <shadab.shadabi786@gmail.com>
2025-10-31 19:57:06 +0530

    Initial commit - HMS Project

---
+++ Doctor/admin.py
@@ -0,0 +1,5 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Doctor
+
+admin.site.register(Doctor)
---
+++ Doctor/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+from django.db import models
+
+# Create your models here.
