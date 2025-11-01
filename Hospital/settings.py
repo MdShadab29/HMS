fe26fac6cdfb18e3f9fc1a16e68e7c42de8c0ccb
Md Shadab <shadab.shadabi786@gmail.com>
2025-11-01 13:21:50 +0530

    Render deploy ready - static and email config added

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -77,3 +77,11 @@ USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "Appointment.Accounts"
+# ✅ Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'neovibe14@gmail.com'
+EMAIL_HOST_PASSWORD = 'ulpqnqoqmxlmkezs'
