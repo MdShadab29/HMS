2408a5954e01b8bc75591124997ca209087cf43a
Md Shadab <shadab.shadabi786@gmail.com>
2025-11-01 12:45:18 +0530

    Fixed static files configuration for Render

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -1,3 +1,11 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEBUG = False
 
 ALLOWED_HOSTS = ['hms-gt67.onrender.com', 'localhost', '127.0.0.1']
